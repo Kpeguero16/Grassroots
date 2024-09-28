9320ed415dfc37b7a8b7a0fc67a578f5381f82ea
Mattjava <77417435+Mattjava@users.noreply.github.com>
2024-09-28 18:06:43 -0400

    Updated script.js
    Implemented the following:
    
    - Search function that prints the Json results with the matching criteria
    - Comments
    
    I expect to fully turn the search function into a function that directly generates a division HTML element for each element found.

--- src/script.js
+++ src/script.js
@@ -5,13 +5,15 @@ function search(input)
 {
     for(let i = 0; i < files.length; i++)
     {
+        // Searches for the term with the matching input
         const file = files[i];
         if(json[file]["color"] === input || json[file]["name"] === input)
         {
+            // Output area
             console.log(json[file]);
         }
     }
 }
 
-search("blue");
+search("green");
 search("John")
